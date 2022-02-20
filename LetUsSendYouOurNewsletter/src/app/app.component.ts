7063562af8d5afe4c1e8c8e7ab53bcd115d734b1
Geetanjali Khabale <gitanjalikhabale@gmail.com>
2022-02-20 19:08:04 +0530

    email validation

--- LetUsSendYouOurNewsletter/src/app/app.component.ts
+++ LetUsSendYouOurNewsletter/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LetUsSendYouOurNewsletter';
+
+  submitted = false;
+  newsLetterForm = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email])
+  }); 
+
+  get email() {
+    return this.newsLetterForm.get('email');
+  }
+
 }
