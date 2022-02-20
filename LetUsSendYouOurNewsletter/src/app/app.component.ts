d93d993707e1c7e9d5832ef8fa3fd1469b91315f
Geetanjali Khabale <gitanjalikhabale@gmail.com>
2022-02-20 20:12:52 +0530

    display browser info

--- LetUsSendYouOurNewsletter/src/app/app.component.ts
+++ LetUsSendYouOurNewsletter/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { fnBrowserDetect } from './helpers/helper';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,43 @@ export class AppComponent {
   submitted = false;
   newsLetterForm = new FormGroup({
     email: new FormControl('',[Validators.required, Validators.email, Validators.pattern('[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}')])
-  }); 
+  });
+  public browserInfo = []; 
+
+  constructor(private modalService: NgbModal) {}
 
   get email() {
     return this.newsLetterForm.get('email');
+  }
+
+  displayBrowserInfo(content) {    
+    this.modalService.open(content, { size: 'sm' });
+    this.browserInfo = [
+      {
+        name: 'Browser Code Name',
+        value: navigator.appCodeName 
+      },
+      {
+        name: 'Browser Name',
+        value: fnBrowserDetect() 
+      },
+      {
+        name: 'Platform',
+        value: navigator.platform
+      },
+      {
+        name: 'Is Cookie Enabled',
+        value: navigator.cookieEnabled
+      },
+      {
+        name: 'Cookie',
+        value: document.cookie
+      }
+    ];
+}
+
+  onSubmit(){
+    console.log(this.newsLetterForm.value);
   }
 
 }
--- LetUsSendYouOurNewsletter/src/app/app.module.ts
+++ LetUsSendYouOurNewsletter/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,    
+    NgbModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
